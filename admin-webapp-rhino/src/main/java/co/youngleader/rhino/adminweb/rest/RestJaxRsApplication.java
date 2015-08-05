6c767df509df18c622cf4186631005a8804b4a5a
dragondwy <dragondwy@gmail.com>
2015-08-06 00:37:58 +0800

    init commit

---
+++ admin-webapp-rhino/src/main/java/co/youngleader/rhino/adminweb/rest/RestJaxRsApplication.java
@@ -0,0 +1,47 @@
+package co.youngleader.rhino.adminweb.rest;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import co.youngleader.rhino.adminweb.rest.resources.MyDemoResource;
+
+public class RestJaxRsApplication extends ResourceConfig {
+
+	/**
+	 * Register JAX-RS application components.
+	 */
+	public RestJaxRsApplication() {
+
+		packages("co.youngleader.rhino.adminweb.rest");
+
+		// // register application resources
+		 register(MyDemoResource.class);
+		// register(PodcastLegacyResource.class);
+		//
+		// // register filters
+		// register(RequestContextFilter.class);
+		// register(LoggingResponseFilter.class);
+		// register(CORSResponseFilter.class);
+		//
+		// // register exception mappers
+		// register(GenericExceptionMapper.class);
+		// register(AppExceptionMapper.class);
+		// register(CustomReasonPhraseExceptionMapper.class);
+		// register(NotFoundExceptionMapper.class);
+		//
+		// // register features
+		// register(JacksonFeature.class);
+		// register(EntityFilteringFeature.class);
+		// EncodingFilter.enableFor(this, GZipEncoder.class);
+
+		// property(EntityFilteringFeature.ENTITY_FILTERING_SCOPE, new
+		// Annotation[] {PodcastDetailedView.Factory.get()});
+
+		// register features
+		register(JacksonFeature.class);
+		register(MultiPartFeature.class);
+
+	}
+
+}
